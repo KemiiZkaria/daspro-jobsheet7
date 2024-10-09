8f89a4d86816452430ee11f927fe3d4e6055749f
KemiiZkaria <ahmadkevinmalik01@gmail.com>
2024-10-09 18:35:53 +0700

    melakukan pembenaran untuk kode percobaan 2

--- SiakadWhile04.java
+++ SiakadWhile04.java
@@ -17,20 +17,20 @@ public class SiakadWhile04 {
                 System.out.println("Nilai tidak valid. Masukkan lagi nilai yang valid!");
                 continue;
             }
-            if (nilai > 80 && nilai > 100) {
+            if (nilai > 80 && nilai <= 100) {
                 System.out.println("Nilai mahasiswa ke-" + (i+1) + " adalah A");
-            } else if (nilai > 73 && nilai > 80) {
-                System.out.println("Nilai mahasiswa ke-" + (i+1) + " adalah A");
-            } else if (nilai > 65 && nilai > 73) {
-                System.out.println("Nilai mahasiswa ke-" + (i+1) + " adalah A");
-            } else if (nilai > 60 && nilai > 65) {
-                System.out.println("Nilai mahasiswa ke-" + (i+1) + " adalah A");
-            } else if (nilai > 50 && nilai > 60) {
-                System.out.println("Nilai mahasiswa ke-" + (i+1) + " adalah A");
-            } else if (nilai > 39 && nilai > 50) {
-                System.out.println("Nilai mahasiswa ke-" + (i+1) + " adalah A");
+            } else if (nilai > 73 && nilai <= 80) {
+                System.out.println("Nilai mahasiswa ke-" + (i+1) + " adalah B+");
+            } else if (nilai > 65 && nilai <= 73) {
+                System.out.println("Nilai mahasiswa ke-" + (i+1) + " adalah B");
+            } else if (nilai > 60 && nilai <= 65) {
+                System.out.println("Nilai mahasiswa ke-" + (i+1) + " adalah C+");
+            } else if (nilai > 50 && nilai <= 60) {
+                System.out.println("Nilai mahasiswa ke-" + (i+1) + " adalah C");
+            } else if (nilai > 39 && nilai <= 50) {
+                System.out.println("Nilai mahasiswa ke-" + (i+1) + " adalah D");
             } else {
-                System.out.println("Nilai mahasiswa ke-" + (i+1) + ": ");
+                System.out.println("Nilai mahasiswa ke-" + (i+1) + " adalah E");
             }
             i++;
         }
