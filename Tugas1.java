b72f1c4292bb81eab32609bf386c802e74ef6aed
KemiiZkaria <ahmadkevinmalik01@gmail.com>
2024-10-10 09:45:56 +0700

    Melakukan push tugas 2

---
+++ Tugas1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Tugas1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int jmlTiket = 0, hargaTiket = 50000, jmlPelanggan, totalHarga, totalTerjual;
+
+        do {
+            System.out.print("Masukkan jumlah pelanggan: ");
+            jmlPelanggan = sc.nextInt();
+        } while (true);
+    }
+}
